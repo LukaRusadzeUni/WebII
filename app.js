821856a4c9155fafb70004503581ada9ea3d7e3d
B15-21 <b15-21@cu.local>
2021-07-02 09:45:12 +0400

    CommonJS Fix

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
-import express from "express";
-import fs from "fs"
-import path from "path";
-import { fileURLToPath } from 'url';
+const express = require("express");
+const fs = require("fs")
+const path = require("path");
+const fileURLToPath = require('url');
 
 const app = express();
 const router = express.Router();
 const pages = fs.readdirSync('./pages').filter(file => file.endsWith('.html'));
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 console.log(pages);
-app.set("port", process.env.PORT || 80);
+app.set("port", process.env.PORT || 3000);
 
 pages.forEach((element) => {
 
     let link;
     element == "index.html" ? link = "" : link = element;
 
-    router.get(`/${link}`, (request, resposne) => {
-        resposne.sendFile(path.resolve(__dirname, `./pages/${element}`))
+    router.get(`/${link}`, (request, response) => {
+        response.sendFile(path.resolve(`./pages/${element}`))
     })
 })
 
 app.use(router)
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.resolve('./public')));
 
 app.listen(app.get("port"), () => {
     console.log("Server started on port " + app.get("port"))
